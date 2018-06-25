c9be4d546e0ee06ff2d50e81a73991491071da08
Daniel Hellwig <daniel.hellwig@live.com>
2018-06-25 17:55:49 +0200

    add quadcopter, del dog-project stuff

---
+++ RL-Quadcopter-2/Untitled-1.py
@@ -0,0 +1,49 @@
+### In [1] ###
+import random
+
+class Basic_Agent():
+    def __init__(self, task):
+        self.task = task
+    
+    def act(self):
+        new_thrust = random.gauss(450., 25.)
+        return [new_thrust + random.gauss(0., 1.) for x in range(4)]
+
+
+### In [2] ###
+%load_ext autoreload
+%autoreload 2
+
+import csv
+import numpy as np
+from task import Task
+
+# Modify the values below to give the quadcopter a different starting position.
+runtime = 5.                                     # time limit of the episode
+init_pose = np.array([0., 0., 10., 0., 0., 0.])  # initial pose # [x, y, z, phi, theta, psi]
+init_velocities = np.array([0., 0., 0.])         # initial velocities # x_velocity, y_velocity, z_velocity
+init_angle_velocities = np.array([0., 0., 0.])   # initial angle velocities # phi_velocity, theta_velocity, psi_velocity
+file_output = 'data.txt'                         # file name for saved results
+
+# Setup
+task = Task(init_pose, init_velocities, init_angle_velocities, runtime)
+agent = Basic_Agent(task)
+done = False
+labels = ['time', 'x', 'y', 'z', 'phi', 'theta', 'psi', 'x_velocity',
+          'y_velocity', 'z_velocity', 'phi_velocity', 'theta_velocity',
+          'psi_velocity', 'rotor_speed1', 'rotor_speed2', 'rotor_speed3', 'rotor_speed4']
+results = {x : [] for x in labels}
+
+# Run the simulation, and save the results.
+with open(file_output, 'w') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(labels)
+    while True:
+        rotor_speeds = agent.act()
+        _, _, done = task.step(rotor_speeds)
+        to_write = [task.sim.time] + list(task.sim.pose) + list(task.sim.v) + list(task.sim.angular_v) + list(rotor_speeds)
+        for ii in range(len(labels)):
+            results[labels[ii]].append(to_write[ii])
+        writer.writerow(to_write)
+        if done:
+            break
